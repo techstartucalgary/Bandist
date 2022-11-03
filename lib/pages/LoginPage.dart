7d9d24a2b30bd98ac4c466692a48a0961c538c13
unknown <nurgul.akhshatayeva@ucalgary.ca>
2022-11-03 13:14:58 -0600

    set front end env

---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to the first screen by popping the current route
+            // off the stack.
+            Navigator.pop(context);
+          },
+          child: const Text('Launch screen'),
+        ),
+      ),
+    );
+  }
+}
