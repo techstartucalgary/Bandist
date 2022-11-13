1ef4d96b2d32466eb3f6fd1fc43d8daff055a76a
Axeloooo <axelshz@gmail.com>
2022-11-12 21:10:37 -0700

    Commit November 12th: Login Page implemented

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,15 +32,36 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login Page Screen'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack.
-            Navigator.pop(context);
-          },
-          child: const Text('Go back to HomePage'),
-        ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            keyboardType: TextInputType.emailAddress,
+            enableSuggestions: false,
+            autocorrect: false,
+            textAlign: TextAlign.center,
+            decoration: const InputDecoration(
+              hintText: "Email",
+            ),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            textAlign: TextAlign.center,
+            decoration: const InputDecoration(
+              hintText: "Password",
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+            }, 
+            child: const Text("Login"),
+            )
+        ],
       ),
     );
   }
