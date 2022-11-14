aa02e7860bb9729f1a8ebda1381f6c046b8f400a
unknown <nurgul.akhshatayeva@ucalgary.ca>
2022-11-14 15:45:05 -0700

    Revert "configure .gitignore, setup deployment"
    This reverts commit 76250958c73c400c5cc0d86657527ab64eac8c29.

--- backend/backendcore/backendcore/wsgi.py
+++ backend/backendcore/backendcore/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendcore.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendcore.settings')
 
 application = get_wsgi_application()
