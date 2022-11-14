018b81f0ed7cd8ea09ef085e3be725e3bbd61bcb
unknown <nurgul.akhshatayeva@ucalgary.ca>
2022-11-14 15:38:58 -0700

    ttettt "Commit November 12th: Login Page implemented"
    This reverts commit fe83bff13c1dac664b19de513ba4034eb8066985.

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,30 +1,7 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,36 +9,15 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: const Text('Login Page Screen'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _email,
-            keyboardType: TextInputType.emailAddress,
-            enableSuggestions: false,
-            autocorrect: false,
-            textAlign: TextAlign.center,
-            decoration: const InputDecoration(
-              hintText: "Email",
-            ),
-          ),
-          TextField(
-            controller: _password,
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            textAlign: TextAlign.center,
-            decoration: const InputDecoration(
-              hintText: "Password",
-            ),
-          ),
-          TextButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
-            }, 
-            child: const Text("Login"),
-            )
-        ],
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to the first screen by popping the current route
+            // off the stack.
+            Navigator.pop(context);
+          },
+          child: const Text('Go back to HomePage'),
+        ),
       ),
     );
   }
