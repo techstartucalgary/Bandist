255c98a0cf7e1e127ab4cff70c46a085330057ed
unknown <nurgul.akhshatayeva@ucalgary.ca>
2022-11-30 20:50:33 -0700

    git push --set-upstream origin nurgul/test2
    Signed off-by: unknown <nurgul.akhshatayeva@ucalgary.ca>

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tech Start | Concert Finder App',
+      title: 'Tech Start | Bandist App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
