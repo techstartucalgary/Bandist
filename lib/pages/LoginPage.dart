66ee8ca20bb8173a60fef323ab1d31cc9707b074
Axeloooo <axelshz@gmail.com>
2022-12-05 21:03:24 -0700

    Login page updated

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -31,100 +31,104 @@ class _LoginPageState extends State<LoginPage> {
     return new Scaffold(
         backgroundColor: Color.fromARGB(255, 255, 255, 255),
         // resizeToAvoidBottomPadding: false,
-        body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-          Container(
-            child: Stack(
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Center(child: Container(
-                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                  child: Text(
-                    'BANDIST',
-                    style:
-                        TextStyle(fontSize: 70.0, fontWeight: FontWeight.bold),
-                  ),
-                ),),
-              ],
-            ),
-          ),
-          SizedBox(height: 20.0),
-          Center(
-            child: Image(image: AssetImage('assets/loginImage.png')),
-          ),
-          Container(
-              padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-              child: Column(
+            Container(
+              child: Stack(
                 children: <Widget>[
-                  SizedBox(height: 50.0),
-                  Container(
-                      height: 50.0,
-                      child: Material(
-                        borderRadius: BorderRadius.circular(20.0),
-                        shadowColor: Colors.blueAccent,
-                        color: Color.fromRGBO(84, 159, 204, 1),
-                        elevation: 7.0,
-                        child: GestureDetector(
-                          onTap: () {},
-                          child: Center(
-                            child: Text(
-                              'LOGIN',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Montserrat'),
+                  Center(child: Container(
+                    padding: EdgeInsets.fromLTRB(15.0, 70.0, 0.0, 0.0),
+                    child: Text(
+                      'BANDIST',
+                      style:
+                          TextStyle(fontSize: 70.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),),
+                ],
+              ),
+            ),
+            //SizedBox(height: 20.0),
+            Expanded(
+              child: Container(
+                child: Image(image: AssetImage('assets/loginImage2.png')),
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.only(top: 0.0, left: 20.0, right: 20.0, bottom: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                        height: 50.0,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          shadowColor: Colors.blueAccent,
+                          color: Color.fromRGBO(84, 159, 204, 1),
+                          elevation: 7.0,
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Center(
+                              child: Text(
+                                'LOGIN',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'),
+                              ),
                             ),
                           ),
-                        ),
-                      )),
-                  SizedBox(height: 25.0),
-                  Container(
-                      height: 50.0,
-                      child: Material(
-                        borderRadius: BorderRadius.circular(20.0),
-                        shadowColor: Colors.blueAccent,
-                        color: Color.fromRGBO(84, 159, 204, 1),
-                        elevation: 7.0,
-                        child: GestureDetector(
-                          onTap: () {},
-                          child: Center(
-                            child: Text(
-                              'SIGNUP',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Montserrat'),
+                        )),
+                    SizedBox(height: 25.0),
+                    Container(
+                        height: 50.0,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          shadowColor: Colors.blueAccent,
+                          color: Color.fromRGBO(84, 159, 204, 1),
+                          elevation: 7.0,
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Center(
+                              child: Text(
+                                'SIGNUP',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'),
+                              ),
                             ),
                           ),
-                        ),
-                      )),
-                  SizedBox(height: 20.0),
-                  Container(
-                    height: 50.0,
-                    color: Colors.transparent,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Colors.black,
-                              style: BorderStyle.solid,
-                              width: 1.0),
-                          color: Colors.transparent,
-                          borderRadius: BorderRadius.circular(20.0)),
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Center(
-                          child: Text('GO BACK',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Montserrat')),
+                        )),
+                    SizedBox(height: 20.0),
+                    Container(
+                      height: 50.0,
+                      color: Colors.transparent,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.black,
+                                style: BorderStyle.solid,
+                                width: 1.0),
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(20.0)),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Center(
+                            child: Text('GO BACK',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat')),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              )),
-        ]));
+                  ],
+                )),
+          ]),
+        ));
   }
 }
