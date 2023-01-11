934bf98d142d98d2c8dd5a50e6e979b5c4f99c99
unknown <nurgul.akhshatayeva@ucalgary.ca>
2023-01-11 05:12:41 -0700

    fixed the cache error

--- backend/bandist/app/urls.py
+++ backend/bandist/app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('login', views.login, name='login'),
     path('login/callback/', views.login_callback, name='login_callback'),
-    path('dashboard', views.dashboard, name='dashboard'),
+    # path('dashboard', views.dashboard, name='dashboard'),
     path('getInfo', views.getInfo, name='get_info'),
 ]
--- backend/bandist/app/views.py
+++ backend/bandist/app/views.py
@@ -3,7 +3,7 @@ from spotipy.oauth2 import SpotifyOAuth
 from django.shortcuts import render, redirect
 from .models import User
 from django.conf import settings
-import time, os
+import time
 
 TOKEN_INFO = "token_info"
 
@@ -15,23 +15,21 @@ def login(request):
         scope='user-read-private user-read-email user-library-read user-follow-read user-top-read',
     )
     login_url = sp_oauth.get_authorize_url()
+
     return redirect(login_url)
 
 def login_callback(request):
+    cache_handler = spotipy.cache_handler.MemoryCacheHandler()
+
     authorization_code = request.GET.get('code')
     sp_oauth = SpotifyOAuth(
         client_id=settings.SPOTIPY_CLIENT_ID,
         client_secret=settings.SPOTIPY_CLIENT_SECRET,
         redirect_uri=settings.SPOTIPY_REDIRECT_URI,
+        cache_handler=cache_handler,
     )
     token_info = sp_oauth.get_access_token(authorization_code)
     request.session.create()
-
-
-    # Create session
-    # if request.session.exists(request.session.session_key):
-        # print('reached here _______________________________')
-        # print(request.session.session_key)
 
     request.session[TOKEN_INFO] = token_info
     return redirect('/getInfo')
@@ -39,11 +37,9 @@ def login_callback(request):
 
 
 def getInfo(request):
-
         
     token_info = request.session.get(TOKEN_INFO, None)
-    print('______________________________________')
-
+   
     if not token_info:
         redirect('/login')
 
@@ -59,82 +55,69 @@ def getInfo(request):
 
     access_token = token_info['access_token']
     sp = spotipy.Spotify(auth=access_token)
-    jcdn = sp.me()
-    print(jcdn)
+    user = sp.current_user()
+
+    try:
+        user = User.objects.get(spotify_id=user['id'])
+        user.save()
+    except User.DoesNotExist:
+        # Create a new User object
+        user = User.objects.create(
+            spotify_id=user['id'],
+            display_name=user['display_name'],
+        )
     top_artists = sp.current_user_top_artists(limit=50, time_range='short_term')
+    follow_artists = sp.current_user_followed_artists(limit=20, after=None)
+    print(follow_artists['artists']['items'][0]['id'])
+    print(follow_artists['artists']['items'][0]['name'])
+    # artist = sp.artist()
     # print(top_artists['items'][0])
-    for artist in top_artists['items']:
-        artist_id = artist['id']
-        # Get the artist's top tracks
-        top_tracks = sp.artist_top_tracks(artist_id)
-        track_ids = [track['id'] for track in top_tracks['tracks']]
-        # Search for events featuring the artist's top tracks
-        # events = sp.events(track_ids=track_ids)
-        # for event in events['events']:
-            # if event['venue']['city'] == city:
-                # concerts.append(event)
-    return render(request, 'dashboard.html', {
-        'top_artists': top_artists['items'],
-    })
-
-    pass
-
-
-# def sakil(request):
-
-#     sp = spotipy.Spotify(auth=access_token)
-#     user = sp.current_user()
-#     spotify_id = user['id']
-#     display_name = user['display_name']
-#     # Check if the city field is present in the data returned by the Spotify API
-#     city = user.get('city', '')
-#     # Check if a User object already exists for this user
-#     try:
-#         user = User.objects.get(spotify_id=spotify_id)
-#         # Update the user's access token
-#         user.access_token = access_token
-#         user.save()
-#     except User.DoesNotExist:
-#         # Create a new User object
-#         user = User.objects.create(
-#             spotify_id=spotify_id,
-#             display_name=display_name,
-#             city=city,
-#             access_token=access_token,
-#             refresh_token=refresh_token,
-#         )
-#     # Store the user's ID in a session or cookie for use in subsequent requests
-#     request.session['user_id'] = spotify_id
-#     return redirect('/dashboard')
-
-
-def dashboard(request):
-    user_id = request.session['user_id']
-    user = User.objects.get(spotify_id=user_id)
-    access_token = user.access_token
-    sp = spotipy.Spotify(auth=access_token)
-    # Get the user's top artists
-    top_artists = sp.current_user_top_artists(limit=50, time_range='short_term')
-    # Get the user's followed artists
-    followed_artists = sp.current_user_followed_artists(limit=50)
-    # Get the upcoming concerts for the user's top artists in their city
-    city = user.city
-    concerts = []
-    for artist in top_artists['items']:
-        artist_id = artist['id']
-        # Get the artist's top tracks
-        top_tracks = sp.artist_top_tracks(artist_id)
-        track_ids = [track['id'] for track in top_tracks['tracks']]
-        # Search for events featuring the artist's top tracks
-        # events = sp.events(track_ids=track_ids)
-        # for event in events['events']:
-            # if event['venue']['city'] == city:
-                # concerts.append(event)
+    # for artist in top_artists['items']:
+    #     artist_id = artist['id']
+    #     # Get the artist's top tracks
+    #     top_tracks = sp.artist_top_tracks(artist_id)
+    #     track_ids = [track['id'] for track in top_tracks['tracks']]
+    #     # Search for events featuring the artist's top tracks
+    #     # events = sp.events(track_ids=track_ids)
+    #     # for event in events['events']:
+    #         # if event['venue']['city'] == city:
+    #             # concerts.append(event)
+    
+  
+    
     return render(request, 'dashboard.html', {
         'display_name': user.display_name,
         'top_artists': top_artists['items'],
-        'followed_artists' : followed_artists,
     })
+
+
+# def dashboard(request):
+#     user_id = request.session['user_id']
+#     user = User.objects.get(spotify_id=user_id)
+#     access_token = user.access_token
+#     sp = spotipy.Spotify(auth=access_token)
+#     # Get the user's top artists
+#     top_artists = sp.current_user_top_artists(limit=50, time_range='short_term')
+#     # Get the user's followed artists
+#     followed_artists = sp.current_user_followed_artists(limit=50)
+#     # Get the upcoming concerts for the user's top artists in their city
+#     city = user.city
+#     concerts = []
+#     for artist in top_artists['items']:
+#         artist_id = artist['id']
+#         # Get the artist's top tracks
+#         top_tracks = sp.artist_top_tracks(artist_id)
+#         track_ids = [track['id'] for track in top_tracks['tracks']]
+#         # Search for events featuring the artist's top tracks
+#         # events = sp.events(track_ids=track_ids)
+#         # for event in events['events']:
+#             # if event['venue']['city'] == city:
+#                 # concerts.append(event)
+#     return render(request, 'dashboard.html', {
+#         'display_name': user.display_name,
+#         'top_artists': top_artists['items'],
+#         'followed_artists' : followed_artists,
+#     })
 
 
 
