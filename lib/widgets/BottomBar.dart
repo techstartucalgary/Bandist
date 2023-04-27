f8b235c1b41fccae9f60dd4b500c6c8cfddf52ac
Levi Sawatzky <levisawatz@gmil.com>
2023-04-27 17:28:21 -0600

    added icon to bottom bar

--- lib/widgets/BottomBar.dart
+++ lib/widgets/BottomBar.dart
@@ -42,19 +42,23 @@ class _BottomBarState extends State<BottomBar> {
               onTap: _onItemTapped,
               items: const [
                 BottomNavigationBarItem(
-                  icon:
-                      Icon(Icons.home, color: Color.fromARGB(255, 90, 88, 89)),
                   label: "Home",
-                ),
+                  icon: Icon(Icons.home, color: Color.fromARGB(255, 90, 88, 89)),),
                 BottomNavigationBarItem(
-                    activeIcon: Icon(Icons.favorite),
-                    icon: Icon(Icons.favorite_outline_sharp),
-                    label: "Search"),
+                    label: "Search",
+                    activeIcon: Icon(Icons.favorite, color: Color.fromARGB(255, 90, 88, 89)),
+                    icon: Icon(Icons.favorite_outline_sharp,  color: Color.fromARGB(255, 90, 88, 89)),),
+                
                 BottomNavigationBarItem(
-                    icon: Icon(Icons.settings),
                     label: "Settings",
+                    icon: Icon(Icons.settings, 
+                        color: Color.fromARGB(255, 90, 88, 89)),
                     activeIcon: Icon(Icons.settings,
-                        color: Color.fromARGB(255, 87, 86, 86)))
+                        color: Color.fromARGB(255, 87, 86, 86))),
+                BottomNavigationBarItem(
+                    label: "new_icon",
+                    icon: Icon(Icons.car_crash_outlined, color: Color.fromARGB(255, 90, 88, 89)),///// N
+                    activeIcon: Icon(Icons.car_crash,)),
               ],
               elevation: 2,
             ),
